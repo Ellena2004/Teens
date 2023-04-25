b7076b1d02d0f761844b4e5b9f677df6b2ff96db
Ellena2004 <cahndydiaries@gmail.com>
2023-04-24 15:43:30 -1100

    update

--- app.js
+++ app.js
@@ -1,58 +1,58 @@
-var express = require("express");
-const path = require('path');
+// var express = require("express");
+// const path = require('path');
 
 
-const app = express();
-app.use(express.static(__dirname + '/public'));
+// const app = express();
+// app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname+'/index.html'));
+// });
 
-app.get("/index", function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+// app.get("/index", function(req, res){
+//     res.sendFile(path.join(__dirname+'/index.html'));
+// });
 
-app.get("/about", function(req, res){
-    res.sendFile(path.join(__dirname+'/about.html'));
-});
+// app.get("/about", function(req, res){
+//     res.sendFile(path.join(__dirname+'/about.html'));
+// });
 
-app.get("/blog", function(req, res){
-    res.sendFile(path.join(__dirname+'/blog.html'));
-});
+// app.get("/blog", function(req, res){
+//     res.sendFile(path.join(__dirname+'/blog.html'));
+// });
 
-app.get("/donate", function(req, res){
-    res.sendFile(path.join(__dirname+'/donate.html'));
-});
+// app.get("/donate", function(req, res){
+//     res.sendFile(path.join(__dirname+'/donate.html'));
+// });
 
-app.get("/blog-single", function(req, res){
-    res.sendFile(path.join(__dirname+'/blog-single.html'));
-});
+// app.get("/blog-single", function(req, res){
+//     res.sendFile(path.join(__dirname+'/blog-single.html'));
+// });
 
-app.get("/contact", function(req, res){
-    res.sendFile(path.join(__dirname+'/contact.html'));
-});
+// app.get("/contact", function(req, res){
+//     res.sendFile(path.join(__dirname+'/contact.html'));
+// });
 
-app.get("/event", function(req, res){
-    res.sendFile(path.join(__dirname+'/event.html'));
-});
+// app.get("/event", function(req, res){
+//     res.sendFile(path.join(__dirname+'/event.html'));
+// });
 
-app.get("/gallery", function(req, res){
-    res.sendFile(path.join(__dirname+'/gallery.html'));
-});
+// app.get("/gallery", function(req, res){
+//     res.sendFile(path.join(__dirname+'/gallery.html'));
+// });
 
-app.get("/Program", function(req, res){
-    res.sendFile(path.join(__dirname+'/programs.html'));
-});
-
-
+// app.get("/Program", function(req, res){
+//     res.sendFile(path.join(__dirname+'/programs.html'));
+// });
 
 
-let  port = process.env.PORT;
-if (port ==null || port == ""){
-    port = 3000;
-}
 
-app.listen(port, function(){
-    console.log("server has started successfully");
-})
+
+// let  port = process.env.PORT;
+// if (port ==null || port == ""){
+//     port = 3000;
+// }
+
+// app.listen(port, function(){
+//     console.log("server has started successfully");
+// })
